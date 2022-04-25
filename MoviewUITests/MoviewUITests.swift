51aaf67d9b49b09f3a9e327ab04e224c3f1db8e3
Alejandro Barbosa <alejobarbosalee@gmail.com>
2022-04-25 17:15:25 -0500

    UI Test added

--- MoviewUITests/MoviewUITests.swift
+++ MoviewUITests/MoviewUITests.swift
@@ -9,34 +9,49 @@ import XCTest
 
 class MoviewUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    override func setUp() {
+        XCUIApplication().launch()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    //MARK: Test Validate Views
+    func testExistViews() {
         let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let tabBar = app.tabBars["Tab Bar"]
+        XCTAssertTrue(tabBar.exists)
+        let moviesItem = tabBar.buttons["Movies"]
+        XCTAssertTrue(moviesItem.exists)
+        let favItem = tabBar.buttons["Favorites"]
+        XCTAssertTrue(favItem.exists)
+        let infoItem = tabBar.buttons["Info"]
+        XCTAssertTrue(infoItem.exists)
+        let tables = app.tables.element
+        XCTAssertTrue(tables.exists)
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+    
+    //MARK: Test Navigation
+    func testTapItemSuccess(){
+        let app = XCUIApplication()
+        let tabBar = app.tabBars["Tab Bar"]
+        tabBar.buttons["Favorites"].tap()
+        let labelFav = app.staticTexts["Your favorite movies"]
+        XCTAssertTrue(labelFav.exists)
+        tabBar.buttons["Info"].tap()
+        let labelInfo = app.staticTexts["Information"]
+        XCTAssertTrue(labelInfo.exists)
+        tabBar.buttons["Movies"].tap()
+        let labelHome =  app.staticTexts["Get to know the most popular movies, and save the ones you like as favorites"]
+        XCTAssertTrue(labelHome.exists)
     }
+    
+    //MARK: Test Navigation
+    ///This test only works with network
+    func testNavigate(){
+        let app = XCUIApplication()
+        sleep(4)
+        let tablesQuery = app.tables
+        tablesQuery.cells.firstMatch.tap()
+        let navBarDetail = app.navigationBars["Moview.MovieDetailView"]
+        XCTAssertTrue(navBarDetail.exists)
+    }
+    
 }
